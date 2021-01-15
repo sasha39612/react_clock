c795b95a12d6620ea8f64182ae35a09cf87b496a
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-15 20:06:59 +0200

    Updates

---
+++ precache-manifest.8b1931c008d508e45ffd91027fe47c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60c65b0e6a5abd7206c157de5040b1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b314cae176e6335d0a87",
+    "url": "./static/css/main.f7350370.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b314cae176e6335d0a87",
+    "url": "./static/js/main.9224f7c1.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
